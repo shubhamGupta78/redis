d9a700f92a7510c18aef6a35dd8a736a6d1aa139
Shubham Gupta <shubhamgupta1999dkg@gmail.com>
2025-06-23 21:55:08 +0530

    my second commit

--- src/main/java/org/example/addtwonumbers/addtwonumbers.java
+++ src/main/java/org/example/addtwonumbers/addtwonumbers.java
@@ -1,9 +1,13 @@
 package org.example.addtwonumbers;
 
+import java.util.Scanner;
+
 public class addtwonumbers {
 
     public static void main(String args[])
     {
-
+        int x,y;
+        Scanner sc = new Scanner(System.in);
+        x=sc.nextInt();
     }
 }
